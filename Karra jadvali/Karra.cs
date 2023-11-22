e63acec1776c2c83a1fec9036cb58a1531fef3a3
alijo <alijo@ALIJONOV>
2023-11-22 22:58:00 +0500

    Add project files.

---
+++ Karra jadvali/Karra.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Karra_jadvali
+{
+    public static class Karra
+    {
+        public static void karraJadvali(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                    Console.Write($"{j} * {i} = {i * j} \t");
+            Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Karra jadvali/Program.cs
@@ -0,0 +1,9 @@
+﻿using Karra_jadvali;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Karra.karraJadvali(n);
+    }
+}
